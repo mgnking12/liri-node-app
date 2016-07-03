1122dca6799ec3706709104427d9eda6d3d0f1ac
Megan King <mgnking12@gmail.com>
2016-07-02 20:24:00 -0500

    inquerer

--- liri.js
+++ liri.js
@@ -1,19 +1,33 @@
 var keys = require('./keys.js');
-var command = process.argv[2];
+var inquirer = require('inquirer');
+//var command = process.argv[2];
 var fs = require('fs');
-var spotArr = [];
-for (var i = 3; i < process.argv.length; i++) {
-    spotArr.push(process.argv[i])
-}
+// var random = [];
+// var spotSong;
+// fs.readFile('./random.txt', 'utf8', function(err, data) {
+//     if (err) {
+//         return console.log(err);
+//     }
+//     var dataARR = data.split(",");
+//     spotSong = dataARR[1];
+//         console.log(spotSong)
+// });
+inquirer.prompt([{
+    type: "list",
+    message: "Make a choice.",
+    choices: ["Twitter", "Music", "Movies", "Never Mind"],
+    name: "action"
+}]).then(function(answers) {
 
-switch (command) {
-    case 'my-tweets':
-        twitter();
-        break;
-    case 'spotify-this-song':
-        spotify();
-        break;
-}
+    switch (answers.action) {
+        case 'Twitter':
+            twitter();
+            break;
+        case 'Music':
+            spotify();
+            break;
+    }
+});
 
 function twitter() {
     var Twitter = require('twitter');
@@ -36,17 +50,29 @@ function twitter() {
 
 function spotify() {
     var spotify = require('spotify');
-    spotify.search({
-        type: 'track',
-        query: spotArr,
-    }, function(err, data) {
-        if (err) {
-            console.log('Error occurred: ' + err);
-            return;
-        }
-        console.log("Song: " + data.tracks.items[0].name);
-        console.log("Artist: " + data.tracks.items[0].artists[0].name);
-        console.log("Album: " + data.tracks.items[0].album.name);
-        console.log("Link: " + data.tracks.items[0].external_urls.spotify);
-    });
+    var spotArr = [];
+
+    inquirer.prompt([{
+        type: "input",
+        message: "What song?",
+        name: "song"
+    }]).then(function(answers) {
+
+        // for (var i = 3; i < process.argv.length; i++) {
+        //     spotArr.push(process.argv[i])
+        // }
+        spotify.search({
+            type: 'track',
+            query: answers.song,
+        }, function(err, data) {
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return;
+            }
+            console.log("Song: " + data.tracks.items[0].name);
+            console.log("Artist: " + data.tracks.items[0].artists[0].name);
+            console.log("Album: " + data.tracks.items[0].album.name);
+            console.log("Link: " + data.tracks.items[0].external_urls.spotify);
+        });
+    })
 }
